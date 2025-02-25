f7c704cc268b7c6dc7999c72a1e08420f2d7020f
ValeriaGC23 <valegc0820@gmail.com>
2025-02-25 17:47:19 -0500

    Add files via upload

---
+++ ejercicio1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int num1;
+	int num2;
+	int num3;
+	int addition;
+
+	cout << "Digite tres numeros" << endl;
+	cin >> num1 >> num2 >> num3;
+
+	if(num1+num2==num3) {
+		cout << num3 << " es la suma de " << num1 << " y " << num2;
+	}
+    else if(num1+num3==num2) {
+		    cout << num2 << " es la suma de " << num1 << " y " << num3;
+	}
+	else if(num2+num3==num1) {
+		cout << num3 << " es la suma de " << num1 << " y " << num2;
+	}
+	else 
+	    cout << "Ningun numero es la suma de los otros dos";
+	
+}
